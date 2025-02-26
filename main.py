db5072449b17632962b7bdf9f9d646835b91facb
Zach Seaborn <zach.seaborn@tklabs.com>
2025-02-26 12:41:36 -0500

    Get file runable

---
+++ main.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    print("Starting....")
+    print("Press 'Ctrl' + 'C' to Quit.")
+    try:
+        pass
+    except KeyboardInterrupt:
+        print("Exiting the Program")
