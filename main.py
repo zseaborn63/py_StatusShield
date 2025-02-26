cb7fcb2eddc8c10932b4880ab596409fe0489eba
Zach Seaborn <zach.seaborn@tklabs.com>
2025-02-26 17:38:54 -0500

    Improve debugging

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ pyautogui.FAILSAFE = False
 def choose_random_number(inclusive_min, exclusive_max):
     """
         Utility function designed to choose a random number between two given numbers.  The lower-bound number will be
-        included in the selection pool but the upper-bound number will be excluded from selection. Utilize the secrets
-        module to be more secure.
+            included in the selection pool but the upper-bound number will be excluded from selection. Utilize the
+            secrets module to be more secure.
 
     :param int inclusive_min: The minimum number that can be returned by this function.
     :param int exclusive_max: The maximum number + 1 that can be returned by this function.
